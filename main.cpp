876350effc8f7729f6e63d251bf216617b87747c
Stan <stan@jos-mac-mini.home>
2024-02-01 16:36:30 +0100

    norme

--- main.cpp
+++ main.cpp
@@ -2,19 +2,17 @@
 
 int main(int argc, char* argv[]) {
     try {
-        if (argc != 2) {
+        if (argc != 2)
             throw std::invalid_argument("Usage: " + std::string(argv[0]) + " <equation>");
-        }
-
+        
         std::string equation = argv[1];
         EquationSolver equationSolver(equation);
 
         std::vector<EquationSolver::Term> parsedTerms = equationSolver.ParseInput();
         std::vector<EquationSolver::Term> reducedTerms = equationSolver.ReduceTerms(parsedTerms);
         std::cout << "Equation Reduced: " << equationSolver.WriteReducedEquation(reducedTerms) << std::endl;
-
         equationSolver.SolvePolynomial(reducedTerms);
-        // system("leaks computorv1");
+        
         return (0);
     } catch (const std::exception& e) {
         std::cerr << e.what() << std::endl;
